327df96fffcae38723bfffda38203f848b575083
甘利聡 <swkaen@amarisatoshi-no-MacBook-Pro.local>
2016-02-15 18:22:24 +0900

    制御機構完成

---
+++ hello.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request, render_template
+import os
+import serial
+
+port ="/dev/tty.usbmodem1421"
+ser = serial.Serial(port, 9600)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('view.html')
+
+@app.route('/a')
+def aaa():
+	print('a押したよ')
+	os.system("say a")
+	ser.write(bytes([0+49]))
+	return render_template('view.html')
+
+@app.route('/b')
+def bbb():
+	print('b押したよ')
+	os.system("say b")
+	ser.write(bytes([1+49]))
+	return render_template('view.html')
+
+@app.route('/c')
+def ccc():
+	print('c押したよ')
+	os.system("say c")
+	ser.write(bytes([2+49]))
+	return render_template('view.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
